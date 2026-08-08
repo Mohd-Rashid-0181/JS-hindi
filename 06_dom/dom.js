cc9f56c7e125f9f8e02174e3a6e2a0cb94d50652
abdulrashid04517-eng <abdulrashid04517@gmail.com>
2026-08-08 17:13:04 +0530

    Dom

---
+++ 06_dom/dom.js
@@ -0,0 +1,8 @@
+// Document for accessing the whole web page 
+// getElementById('') for accessing the element from their id 
+// innerText for accessing the text only who is displaying 
+// textContent for accessing the whole text even also that text who is not showing at all because of their css property
+// innerHTML for accessing andchanging the whole html 
+// querySelector for accessing the anything like element, attribute by their id or class name if we access by their id then we apply # before name and if we access by class then we apply (.) before name and if there is multiple of that name then this selector give us first and afterward don't
+// querySelectorAll for accessing the multiple of same name and work like querySelector
+// for converting the nodelist or htmlcollection into pure array we use (from())function with array liike =>  Array.From(htmlcollection)
